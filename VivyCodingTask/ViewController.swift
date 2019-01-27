3ed9584cab48a3f621195a562d45c902a5202ae2
Miso Lubarda <miso.lubarda@here.com>
2019-01-27 19:01:35 +0100

    Initial Commit

---
+++ VivyCodingTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VivyCodingTask
+//
+//  Created by Lubarda, Miso on 27.01.19.
+//  Copyright © 2019 Lubarda, Miso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
