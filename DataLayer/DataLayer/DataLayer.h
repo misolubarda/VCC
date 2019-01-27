45deb1be1da1768988181e17afb406a41e79ec62
Miso Lubarda <miso.lubarda@here.com>
2019-01-27 19:17:28 +0100

    Add empty domain and data layer frameworks

---
+++ DataLayer/DataLayer/DataLayer.h
@@ -0,0 +1,19 @@
+//
+//  DataLayer.h
+//  DataLayer
+//
+//  Created by Lubarda, Miso on 27.01.19.
+//  Copyright © 2019 Lubarda, Miso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataLayer.
+FOUNDATION_EXPORT double DataLayerVersionNumber;
+
+//! Project version string for DataLayer.
+FOUNDATION_EXPORT const unsigned char DataLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataLayer/PublicHeader.h>
+
+
---
+++ DomainLayer/DomainLayer/DomainLayer.h
@@ -0,0 +1,19 @@
+//
+//  DomainLayer.h
+//  DomainLayer
+//
+//  Created by Lubarda, Miso on 27.01.19.
+//  Copyright © 2019 Lubarda, Miso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DomainLayer.
+FOUNDATION_EXPORT double DomainLayerVersionNumber;
+
+//! Project version string for DomainLayer.
+FOUNDATION_EXPORT const unsigned char DomainLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DomainLayer/PublicHeader.h>
+
+
