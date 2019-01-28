c0c6aecc407fdf6ede70c0be3caefab4c9d5f6d0
Miso Lubarda <miso.lubarda@here.com>
2019-01-28 16:05:33 +0100

    Clean up UI layer
    Basic folder structure. AppDelegate clean up. Storyboards removed because
    they won't be used.

---
+++ VivyCodingTask/UI/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  VivyCodingTask
+//
+//  Created by Lubarda, Miso on 27.01.19.
+//  Copyright © 2019 Lubarda, Miso. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
