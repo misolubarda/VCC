fd5ac05e694aef1df0089059f8c88c835fef4156
Miso Lubarda <miso.lubarda@here.com>
2019-01-29 16:03:07 +0100

    Add button to navigation bar to be able to get current country info

--- VivyCodingTask/UI/AppCoordinator.swift
+++ VivyCodingTask/UI/AppCoordinator.swift
@@ -24,8 +24,23 @@ class AppCoordinator {
     }
 
     func start() {
-        let countryListVC = CountryListViewController(dependencies: dependencies)
+        let countryListVC = getCountryListVC(dependencies: dependencies)
         window.rootViewController = UINavigationController(rootViewController: countryListVC)
         window.makeKeyAndVisible()
+    }
+
+    private func getCountryListVC(dependencies: CountryListViewControllerDependencies) -> CountryListViewController {
+        let countryListVC = CountryListViewController(dependencies: dependencies)
+        countryListVC.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "My Country", style: .plain, target: self, action: #selector(myCountryButtonTapped))
+
+        return countryListVC
+    }
+}
+
+// MARK: Navigation item button action
+
+extension AppCoordinator {
+    @objc func myCountryButtonTapped() {
+
     }
 }
